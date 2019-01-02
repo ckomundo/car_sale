d7ec3c81ce908ff684b0e0866da6687071c0b402
joas97 <aldapa97@gmail.com>
2019-01-02 14:39:52 -0700

    Actualizacion

---
+++ model/db.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'car_sale'
+});
+
+connection.connect(function(err){
+	if(err) throw err;
+});
+
+module.exports = connection;
